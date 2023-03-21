ffa9d9042e34fa7faa6fba09e1b4df771565e178
CS Project Y1 T1 <cs1112@exeter.ac.uk>
2023-03-21 11:48:30 +0000

    fixed tests formatting

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -3,11 +3,12 @@ import socialmedia.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SocialMediaPlatformTestApp {
+	private static SocialMediaPlatform platform = new SocialMedia();
 
 	// Tests For createAccount:
 	@Test
 	public void createAccount_InvalidHandleExceptionFromEmptyHandle() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("");
 		});
@@ -15,7 +16,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void createAccount_InvalidHandleExceptionFromWhiteSpaces() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("Handle Test");
 		});
@@ -23,15 +24,15 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void createAccount_InvalidHandleExceptionFromTooLong() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 		});
 	}
 
 	@Test
-	public void createAccount_IllegalHandleExceptionFromHandleAlreadyExisting() {
-		SocialMediaPlatform platform = new SocialMedia();
+	public void createAccount_IllegalHandleExceptionFromHandleAlreadyExisting() throws HandleNotRecognisedException {
+		platform.erasePlatform();
 		assertThrows(IllegalHandleException.class, () -> {
 			platform.createAccount("Account1");
 			platform.createAccount("Account1");
@@ -40,23 +41,18 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void createAccount_CheckIncrementalId() throws IllegalHandleException, InvalidHandleException, HandleNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id1 = platform.createAccount("Account1");
 		assertEquals (id1 + 1, platform.createAccount("Account2"));
 		assertEquals (id1 + 2, platform.createAccount("Account3"));
 		assertEquals (id1 + 3, platform.createAccount("Account4"));
-
-		platform.removeAccount("Account1");
-		platform.removeAccount("Account2");
-		platform.removeAccount("Account3");
-		platform.removeAccount("Account4");
 	}
 
 
 	// Tests for removeAccount:
 	@Test
 	public void removeAccount_AccountIDNotRecognisedException() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(AccountIDNotRecognisedException.class, () -> {
 			platform.createAccount("Account1");
 			platform.removeAccount(-1);
@@ -65,7 +61,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void removeAccount_TestIfAnAccountIsRemoved() throws IllegalHandleException, InvalidHandleException, AccountIDNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id = platform.createAccount("Account1");
 		assertEquals(1, platform.getNumberOfAccounts());
 		platform.removeAccount(id);
@@ -74,7 +70,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void removeAccount_TestIfTheCorrectAccountIsRemoved() throws IllegalHandleException, InvalidHandleException, AccountIDNotRecognisedException, HandleNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id1 = platform.createAccount("Account1");
 		int id2 = platform.createAccount("Account2");
 		int id3 = platform.createAccount("Account3");
@@ -84,15 +80,12 @@ public class SocialMediaPlatformTestApp {
 			platform.showAccount("Account2");
 		});
 		assertEquals(2, platform.getNumberOfAccounts());
-
-		platform.removeAccount("Account1");
-		platform.removeAccount("Account3");
 	}
 
 	// Tests for changeAccountHandle
 	@Test
 	public void changeAccountHandle_HandleNotRecognisedException() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(HandleNotRecognisedException.class, () -> {
 			platform.createAccount("Account1");
 			platform.changeAccountHandle("Account2", "NewAccount1");
@@ -101,7 +94,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void changeAccountHandle_InvalidNewHandleExceptionFromEmptyHandle() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("Account1");
 			platform.changeAccountHandle("Account1", "");
@@ -110,7 +103,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void changeAccountHandle_InvalidNewHandleExceptionFromWhiteSpaces() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("Account1");
 			platform.changeAccountHandle("Account1", "Handle Test");
@@ -119,7 +112,7 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void changeAccountHandle_InvalidNewHandleExceptionFromTooLong() {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(InvalidHandleException.class, () -> {
 			platform.createAccount("Account1");
 			platform.changeAccountHandle("Account1", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
@@ -128,19 +121,17 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void changeAccountHandle_TestIfAccountIsChanged() throws IllegalHandleException, InvalidHandleException, HandleNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id1 = platform.createAccount("Account1");
 		assertEquals("ID: "+id1+"\nHandle: Account1\nDescription: \nPost count: 0\nEndorse count: 0", platform.showAccount("Account1"));
 		platform.changeAccountHandle("Account1", "Account2");
 		assertEquals("ID: "+id1+"\nHandle: Account2\nDescription: \nPost count: 0\nEndorse count: 0", platform.showAccount("Account2"));
-
-		platform.removeAccount("Account2");
 	}
 
 	//Tests for updateAccountDescription
 	@Test
 	public void updateAccountDescription_HandleNotRecognisedException(){
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(HandleNotRecognisedException.class, () -> {
 			platform.createAccount("Account1");
 			platform.updateAccountDescription("Account2", "NewAccount1");
@@ -149,19 +140,17 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void updateAccountDescription_TestIfDescriptionIsChanged() throws IllegalHandleException, InvalidHandleException, HandleNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id1 = platform.createAccount("Account1", "Old Description");
 		assertEquals("ID: "+id1+"\nHandle: Account1\nDescription: Old Description\nPost count: 0\nEndorse count: 0", platform.showAccount("Account1"));
 		platform.updateAccountDescription("Account1", "New Description");
 		assertEquals("ID: "+id1+"\nHandle: Account1\nDescription: New Description\nPost count: 0\nEndorse count: 0", platform.showAccount("Account1"));
-
-		platform.removeAccount("Account1");
 	}
 
 	//Tests for showAccount
 	@Test
 	public void showAccount_HandleNotRecognisedException(){
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		assertThrows(HandleNotRecognisedException.class, () -> {
 			platform.createAccount("Account1");
 			platform.showAccount("Account2");
@@ -170,11 +159,9 @@ public class SocialMediaPlatformTestApp {
 
 	@Test
 	public void showAccount_TestIfAccountIsShown() throws IllegalHandleException, InvalidHandleException, HandleNotRecognisedException {
-		SocialMediaPlatform platform = new SocialMedia();
+		platform.erasePlatform();
 		int id1 = platform.createAccount("Account1", "Old Description");
 		assertEquals("ID: "+id1+"\nHandle: Account1\nDescription: Old Description\nPost count: 0\nEndorse count: 0", platform.showAccount("Account1"));
-
-		platform.removeAccount("Account1");
 	}
 
 	//Tests for createPost
